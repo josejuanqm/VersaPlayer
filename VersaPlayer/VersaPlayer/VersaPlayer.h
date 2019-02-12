7a13461987790356e9a7a46f7813d799af4dcec7
Philipp Rembold <philipp@macuja.com>
2019-02-12 17:18:04 +0100

    adds a framework for carthage

---
+++ VersaPlayer/VersaPlayer/VersaPlayer.h
@@ -0,0 +1,18 @@
+//
+//  VersaPlayer.h
+//  VersaPlayer
+//
+//  Created by Philipp Rembold on 12.02.19.
+//  Copyright © 2019 Boards & More. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VersaPlayer.
+FOUNDATION_EXPORT double VersaPlayerVersionNumber;
+
+//! Project version string for VersaPlayer.
+FOUNDATION_EXPORT const unsigned char VersaPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VersaPlayer/PublicHeader.h>
+
