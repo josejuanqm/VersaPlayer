512aa4246986a8c00a3cfb4fe23139033b2b0e01
Erekle <erekle@itdc.ge>
2019-02-28 12:20:11 +0400

    fixes

--- VersaPlayer/Classes/Source/Core/Rendering/VersaPlayerRenderingView.swift
+++ VersaPlayer/Classes/Source/Core/Rendering/VersaPlayerRenderingView.swift
@@ -14,56 +14,45 @@ import UIKit
 import AVKit
 
 open class VersaPlayerRenderingView: View {
-    
-    /// VPlayerLayer instance used to render player content
-    public var renderingLayer: VersaPlayerLayer!
-    
-    /// VersaPlayer instance being rendered by renderingLayer
-    public weak var player: VersaPlayerView!
 
-    deinit {
-      #if DEBUG
-          print("6 \(String(describing: self))")
-      #endif
-    }
+  #if os(iOS)
+  override open class var layerClass: AnyClass {
+    return AVPlayerLayer.self
+  }
+  #endif
 
-    /// Constructor
-    ///
-    /// - Parameters:
-    ///     - player: VersaPlayer instance to render.
-    public init(with player: VersaPlayerView) {
-        super.init(frame: CGRect.zero)
-        self.player = player
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    #if os(macOS)
-    
-    open override func layout() {
-        super.layout()
-        if renderingLayer == nil {
-            renderingLayer = VersaPlayerLayer.init(with: player)
-            layer = renderingLayer.playerLayer
-        }
-        
-        renderingLayer.playerLayer.frame = bounds
-    }
-    
-    #else
-    
-    open override func layoutSubviews() {
-        super.layoutSubviews()
-        if renderingLayer == nil {
-            renderingLayer = VersaPlayerLayer.init(with: player)
-            layer.addSublayer(renderingLayer.playerLayer)
-        }
-        
-        renderingLayer.playerLayer.frame = bounds
-    }
-    
+  public var playerLayer: AVPlayerLayer {
+    return layer as! AVPlayerLayer
+  }
+
+  /// VersaPlayer instance being rendered by renderingLayer
+  public weak var player: VersaPlayerView!
+
+  deinit {
+    #if DEBUG
+    print("6 \(String(describing: self))")
     #endif
-    
+  }
+
+  /// Constructor
+  ///
+  /// - Parameters:
+  ///     - player: VersaPlayer instance to render.
+  public init(with player: VersaPlayerView) {
+    super.init(frame: CGRect.zero)
+    playerLayer.player = player.player
+  }
+
+  required public init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  #if os(macOS)
+
+  override open func makeBackingLayer() -> CALayer {
+    return playerLayer
+  }
+
+  #endif
+
 }
